93ba57657e0db5f331068d6668eb2b63af7ea4ea
Sergey O. Boyko <sergey.boyko0791@gmail.com>
2021-12-20 19:25:53 +0700

    Push missing files

---
+++ mm2src/coins_activation/src/utxo_activation/init_utxo_standard_activation.rs
@@ -0,0 +1,105 @@
+use crate::context::CoinsActivationContext;
+use crate::prelude::TryFromCoinProtocol;
+use crate::standalone_coin::InitStandaloneCoinActivationOps;
+use crate::utxo_activation::init_utxo_standard_activation_error::InitUtxoStandardError;
+use crate::utxo_activation::init_utxo_standard_statuses::{UtxoStandardAwaitingStatus, UtxoStandardInProgressStatus,
+                                                          UtxoStandardUserAction};
+use crate::utxo_activation::utxo_standard_activation_result::UtxoStandardActivationResult;
+use async_trait::async_trait;
+use coins::utxo::utxo_standard::UtxoStandardCoin;
+use coins::utxo::{utxo_common, UtxoActivationParams};
+use coins::{CoinProtocol, MarketCoinOps, PrivKeyBuildPolicy};
+use common::mm_ctx::MmArc;
+use common::mm_error::prelude::*;
+use common::Future01CompatExt;
+use crypto::trezor::trezor_rpc_task::RpcTaskHandle;
+use rpc_task::RpcTaskManagerShared;
+use serde_json::Value as Json;
+use std::collections::HashMap;
+
+pub type UtxoStandardTaskManagerShared = RpcTaskManagerShared<
+    UtxoStandardActivationResult,
+    InitUtxoStandardError,
+    UtxoStandardInProgressStatus,
+    UtxoStandardAwaitingStatus,
+    UtxoStandardUserAction,
+>;
+
+pub type UtxoStandardRpcTaskHandle = RpcTaskHandle<
+    UtxoStandardActivationResult,
+    InitUtxoStandardError,
+    UtxoStandardInProgressStatus,
+    UtxoStandardAwaitingStatus,
+    UtxoStandardUserAction,
+>;
+
+pub struct UtxoStandardProtocolInfo;
+
+impl TryFromCoinProtocol for UtxoStandardProtocolInfo {
+    fn try_from_coin_protocol(proto: CoinProtocol) -> Result<Self, MmError<CoinProtocol>>
+    where
+        Self: Sized,
+    {
+        match proto {
+            CoinProtocol::UTXO => Ok(UtxoStandardProtocolInfo),
+            protocol => MmError::err(protocol),
+        }
+    }
+}
+
+#[async_trait]
+impl InitStandaloneCoinActivationOps for UtxoStandardCoin {
+    type ActivationRequest = UtxoActivationParams;
+    type StandaloneProtocol = UtxoStandardProtocolInfo;
+    type ActivationResult = UtxoStandardActivationResult;
+    type ActivationError = InitUtxoStandardError;
+    type InProgressStatus = UtxoStandardInProgressStatus;
+    type AwaitingStatus = UtxoStandardAwaitingStatus;
+    type UserAction = UtxoStandardUserAction;
+
+    fn rpc_task_manager(activation_ctx: &CoinsActivationContext) -> &UtxoStandardTaskManagerShared {
+        &activation_ctx.init_utxo_standard_task_manager
+    }
+
+    async fn init_standalone_coin(
+        ctx: MmArc,
+        ticker: String,
+        coin_conf: Json,
+        activation_request: Self::ActivationRequest,
+        _protocol_info: Self::StandaloneProtocol,
+        priv_key_policy: PrivKeyBuildPolicy<'_>,
+        _task_handle: &UtxoStandardRpcTaskHandle,
+    ) -> MmResult<Self, InitUtxoStandardError> {
+        utxo_common::utxo_arc_from_conf_and_params(
+            &ctx,
+            &ticker,
+            &coin_conf,
+            activation_request,
+            priv_key_policy,
+            UtxoStandardCoin::from,
+        )
+        .await
+        .mm_err(|e| InitUtxoStandardError::from_build_err(e, ticker))
+    }
+
+    // TODO finish implementing this method.
+    async fn get_activation_result(
+        &self,
+        _task_handle: &UtxoStandardRpcTaskHandle,
+    ) -> MmResult<Self::ActivationResult, InitUtxoStandardError> {
+        let current_block =
+            self.current_block()
+                .compat()
+                .await
+                .map_to_mm(|error| InitUtxoStandardError::CoinCreationError {
+                    ticker: self.ticker().to_owned(),
+                    error,
+                })?;
+        let result = UtxoStandardActivationResult {
+            current_block,
+            addresses_infos: HashMap::new(),
+        };
+
+        Ok(result)
+    }
+}
---
+++ mm2src/coins_activation/src/utxo_activation/mod.rs
@@ -0,0 +1,6 @@
+mod init_utxo_standard_activation;
+mod init_utxo_standard_activation_error;
+mod init_utxo_standard_statuses;
+mod utxo_standard_activation_result;
+
+pub use init_utxo_standard_activation::UtxoStandardTaskManagerShared;
