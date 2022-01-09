584ad3c52a04806d8c77ee98e60454ed8693023c
Sergey Boyko <sergey.boyko0791@gmail.com>
2022-01-09 20:05:18 +0700

    Fix rustfmt warnings

--- mm2src/coins_activation/src/standalone_coin/mod.rs
+++ mm2src/coins_activation/src/standalone_coin/mod.rs
@@ -1,6 +1,6 @@
 mod init_standalone_coin;
 mod init_standalone_coin_error;
 
-pub use init_standalone_coin::{init_standalone_coin, init_standalone_coin_status, InitStandaloneCoinActivationOps,
-                               InitStandaloneCoinInitialStatus, init_standalone_coin_user_action};
+pub use init_standalone_coin::{init_standalone_coin, init_standalone_coin_status, init_standalone_coin_user_action,
+                               InitStandaloneCoinActivationOps, InitStandaloneCoinInitialStatus};
 pub use init_standalone_coin_error::InitStandaloneCoinError;
